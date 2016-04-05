95f9ddb62e64abb7dac1188bd55a5558bc363f10
susandaiute <susandaiute@gmail.com>
2016-04-05 13:47:24 -0400

    Start on Gameplay Javascript

---
+++ assets/scripts/gameplay.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let originalBoard = [0,1,2,3,4,5,6,7,8];
+
+
+let winningCombos =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+let squareUsed = [];
+
+let playerXChoice;
+let playerYChoice;
+let playerTurn=1;
+
+let x = "X";
+let o = "O";
+
+const boxClicked = function(boxId){
+  document.getElementById(boxId).text == "X";
+};
+
+
+
+
+
+
+
+const checkWin = function(){};
+
+
+
+
+module.exports=true;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,11 +5,23 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+require('./gameplay'):
 
-$(document).ready(function(){
-  $('.save-btn').on('click', function(e){
+
+//Sign In
+$(document).ready(function() {
+  $('.save-btn').on('click', function(e) {
     e.preventDefault();
     let text = $('#exampleInputEmail1').val();
+    console.log(text);
+  });
+});
+
+//Sign Up
+$(document).ready(function() {
+  $('.save-btn').on('click', function(e) {
+    e.preventDefault();
+    let text = $('#exampleInputEmailsignUp').val();
     console.log(text);
   });
 });
