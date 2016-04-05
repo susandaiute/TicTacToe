812aaacae2ed85d60827b5f8dbe262c4131e80ed
susandaiute <susandaiute@gmail.com>
2016-04-05 14:01:42 -0400

    Create function "getPlayer"
    getPlayer is tested and functional

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -8,11 +8,22 @@ let winningCombos =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 let squareUsed = [];
 
 let playerXChoice;
-let playerYChoice;
+let playerOChoice;
 let playerTurn=1;
+let whoPlay;
 
-let x = "X";
-let o = "O";
+let playerX;
+let playerY;
+
+const getPlayer = function() {
+  if(playerTurn%2===0){
+    playerY;
+  } else {
+    playerX;
+  }
+};
+
+
 
 const boxClicked = function(boxId){
   document.getElementById(boxId).text == "X";
